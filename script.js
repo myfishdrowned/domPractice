33fb2787070997d4dd351f828aeba3ebade845c5
myfishdrowned <saif.rahman.va@gmail.com>
2024-12-22 16:26:31 -0500

    practiced styling text in webpage using js

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
-const body = document.body
-body.append('hello world')
+document.title = 'working with the DOM';
+document.body.style.backgroundColor = '#181818';
+// document.body.style.color = 'silver';
+document.getElementById('paragraph').style.color = 'silver';
+
+const newElement = document.createElement('div');
+
+newElement.id = 'greeting';
+
+newElement.textContent = 'hello world';
+
+document.body.append(newElement);
+
+document.getElementById('greeting').style.color = 'green';
