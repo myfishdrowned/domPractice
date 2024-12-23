b6fc7e9c5fad9c9c5aaca431b169aa74c766c309
myfishdrowned <saif.rahman.va@gmail.com>
2024-12-22 22:39:06 -0500

    practiced manipulating the DOM using javascript

--- script.js
+++ script.js
@@ -1,14 +1,38 @@
-document.title = 'working with the DOM';
-document.body.style.backgroundColor = '#181818';
-// document.body.style.color = 'silver';
-document.getElementById('paragraph').style.color = 'silver';
+// document.title = 'working with the DOM';
+// document.body.style.backgroundColor = '#181818';
+// // document.body.style.color = 'silver';
+// document.getElementById('paragraph').style.color = 'silver';
 
-const newElement = document.createElement('div');
+// const newElement = document.createElement('div');
 
-newElement.id = 'greeting';
+// newElement.id = 'greeting';
 
-newElement.textContent = 'hello world';
+// newElement.textContent = 'hello world';
 
-document.body.append(newElement);
+// document.body.append(newElement);
 
-document.getElementById('greeting').style.color = 'green';
+// document.getElementById('greeting').style.color = 'green';
+
+
+const redText = document.createElement('p')
+redText.textContent = 'Hey I\'m red!'
+console.log(redText);
+
+const blueText = document.createElement('h3')
+blueText.textContent = 'I\'m a blue h3!'
+console.log(blueText);
+
+const pink = document.createElement('div')
+pink.textContent = 'I\'m pink'
+pink.style.border = '2px solid black';
+pink.style.backgroundColor = 'hotpink';
+const header = document.createElement('h1')
+header.textContent = 'I\'m in a div'
+const paragraph = document.createElement('p')
+paragraph.textContent = 'ME TOO'
+pink.appendChild(header)
+pink.appendChild(paragraph)
+document.body.appendChild(pink)
+console.log(pink);
+
+
